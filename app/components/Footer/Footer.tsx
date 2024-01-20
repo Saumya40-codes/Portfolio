124a64af10abdf5f6e2c5efa1d78bd1ebb4ff449
Saumya <saumyabshah90@gmail.com>
2024-01-20 23:13:05 +0530

    new: Footer section

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import styles from './footer.module.css'
 
 const Footer = () => {
   return (
-    <div>
-      Made with Next.js and Vanilla CSS
+    <div className={styles.foot}>
+      <span className={styles.made}>
+      Made with <span className={styles.mainLang}>Next.js</span> and <span className={styles.mainLang}>Vanilla CSS</span>
+      </span>
+      <div className={styles.foot}>
+        <h3 className={styles.made}>
+            Send me a message
+        </h3>
+
+        <div>
+        <span className={styles.made}>Name: </span> <input title='text' type='text' className={styles.input} />
+        </div>
+        <div>
+        <span className={styles.messName}>Message: </span> <textarea title='text' className={styles.mess} />
+        </div>
+
+        <button className={styles.btn}>Send</button>
+       </div>
     </div>
   )
 }
